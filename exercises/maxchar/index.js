3dddf79a88f12ee72cf7f31c5de11ff7b03c5363
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-10 17:04:00 +0000

    maxChar-> Add for in loop

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -18,7 +18,13 @@ function maxChar(str) {
             charMap[char] = 1;
         }
     }
-
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char; 
+        }
+    }
+    return maxChar;
 }
 
 module.exports = maxChar;
