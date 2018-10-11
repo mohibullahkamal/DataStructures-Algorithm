4c7ee99ab4806721e530820d297f6c0a5c64b9dc
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-11 19:30:49 +0000

    chunk-> using .slice() for second solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,17 +10,45 @@
 
 function chunk(array, size) {
     const chunked = [];
+    let index = 0;
     
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-        
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
-   return chunked; 
+    return chunked;
 }
 
 module.exports = chunk;
+
+
+// //******** First solution *********
+// function chunk(array, size) {
+//     const chunked = [];
+    
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+        
+//         if (!last || last.length === size) {
+//             chunked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//   return chunked; 
+// }
+
+
+// //******** Second solution *********
+// function chunk(array, size) {
+//     const chunked = [];
+//     let index = 0;
+    
+//     while (index < array.length) {
+//         chunked.push(array.slice(index, index + size));
+//         index += size;
+//     }
+//     return chunked;
+// }
+
+
