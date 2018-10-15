60fa368b3f112227a6d5b21a6b8a723d358911a5
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-15 16:39:47 +0000

    Update Readme

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,4 +1,4 @@
-// --- Directions
+// // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
@@ -19,11 +19,11 @@ module.exports = reverse;
 
 
 
-// *************************************
-// *************************************
-// *************************************
-// THIRD SOLUTION:
-// *************************************
+// // *************************************
+// // *************************************
+// // *************************************
+// // THIRD SOLUTION:
+// // *************************************
 // // Below solution uses ES5 syntax... "reduce()" helper function was introduced in ES5.1 to reduce all different values in an ARRAY and condense them all to one singular value... "reduce()" takes two separate arguments... the first one is gonna be an arrow function; second one is gonna be a starting argument...
 
 // function reverse(str) {
@@ -31,38 +31,40 @@ module.exports = reverse;
 // }
 
 
-// *************************************
-// *************************************
-// *************************************
-// SECOND SOLUTION:
-// *************************************
-// for loop, we are going to create an
-// empty string called "reversed".. uses "for of" loop -> style, instead of "for()" loop -> style
-//
-// let a = '';
-//
-// for (let x of str) {
-//   a = x + a;
-// } return a;
+// // *************************************
+// // *************************************
+// // *************************************
+// // SECOND SOLUTION:
+// // *************************************
+// // for loop, we are going to create an
+// // empty string called "reversed".. uses "for of" loop -> style, instead of "for()" loop -> style
+
+// function reverse(str) {
+//     let a = '';
+    
+//     for (let x of str) {
+//       a = x + a;
+//     } return a;
+// }
 
 
-// *************************************
-// *************************************
-// *************************************
-// FIRST SOLUTION:
-// *************************************
+// // *************************************
+// // *************************************
+// // *************************************
+// // FIRST SOLUTION:
+// // *************************************
 // // we break the string into array, then reverse it, then
 // // remake the array into a string..
 // // lets do it using for for loop/array helper because
 // // interviewer might say
 // // don't cheat by using the reverse() helper...
 
-//  var arr = str.split('');   // when we pass ('') empty string... it takes the provided string and turns each character in the "str" to array... 
-// arr.reverse();   //This will reverse all character in the array
-// var reversed = arr.join('');   //This will join back all string
-// return reversed;
-
-
+// function reverse(str) {
+//     var arr = str.split('');   // when we pass ('') empty string... it takes the provided string and turns each character in the "str" to array... 
+//     arr.reverse();   //This will reverse all character in the array
+//     var reversed = arr.join('');   //This will join back all string
+//     return reversed;
+// }
 
 // // lets write the above code in a shorter manner..
 // // we dont really need temp var "arr"..
