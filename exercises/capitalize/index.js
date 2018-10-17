78158823414378fb2b7ad4fd10d80aee91a3140d
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-17 11:10:10 +0000

    Work on Sentence Capitalization

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -10,3 +10,36 @@
 function capitalize(str) {}
 
 module.exports = capitalize;
+
+
+
+
+// ***********************************
+// ****** First Solution *************
+// ***********************************
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+// ***********************************
+// ****** Second Solution ************
+// ***********************************
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
+}
