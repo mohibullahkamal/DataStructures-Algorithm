00a4c4e99c294635dd649fa810fc89e675b93dc1
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-17 11:11:45 +0000

    Comment out trials

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -9,37 +9,39 @@
 
 function capitalize(str) {}
 
+
+
 module.exports = capitalize;
 
 
 
 
-// ***********************************
-// ****** First Solution *************
-// ***********************************
-function capitalize(str) {
-  let result = str[0].toUpperCase();
+// // ***********************************
+// // ****** First Solution *************
+// // ***********************************
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
 
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === ' ') {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    }
-  }
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-// ***********************************
-// ****** Second Solution ************
-// ***********************************
-function capitalize(str) {
-  const words = [];
+// // ***********************************
+// // ****** Second Solution ************
+// // ***********************************
+// function capitalize(str) {
+//   const words = [];
 
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1));
-  }
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
 
-  return words.join(' ');
-}
+//   return words.join(' ');
+// }
